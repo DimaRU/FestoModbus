60b5838f194eb456c5a9a4ad5f55e04b3dfebcd1
Dmitriy Borovikov <dmitry@borovikov.name>
2021-12-02 01:29:23 +0300

    direct cmd

--- Sources/FestoModbus/FestoModbus.swift
+++ Sources/FestoModbus/FestoModbus.swift
@@ -58,6 +58,7 @@ public class FestoModbus {
 
     func makeDirectModeRequest(ccon: CCON, cpos: CPOS, cdir: CDIR, v1: UInt8, v2: UInt32) -> [UInt16] {
         var request: [UInt16] = .init(repeating: 0, count: 4)
+        logger.trace("Send\n\(ccon)\n\(cpos)\n\(cdir)\n v1=\(v1) v2=\(v2)")
         request.withUnsafeMutableBytes { ptr in
             ptr[0] = ccon.rawValue
             ptr[1] = cpos.rawValue
@@ -91,6 +92,7 @@ public class FestoModbus {
             let sdir = SDIR(rawValue: ptr[2])
             let v1 = ptr[3]
             let v2 = ptr.load(fromByteOffset: 4, as: UInt32.self)
+            logger.trace("Receive\n\(scon)\n\(spos)\n\(sdir)\n v1=\(v1) v2=\(v2)")
             return (scon, spos, sdir, v1, v2)
         }
     }
@@ -104,6 +106,18 @@ public class FestoModbus {
             throw FestoError.faultOrWarn
         }
         return (scon, spos, rsb)
+    }
+
+    private func sendDirectCmd(ccon: CCON, cpos: CPOS, cdir: CDIR = [], v1: UInt8 = 0, v2: UInt32 = 0) throws ->
+                                            (scon: SCON, spos: SPOS, sdir: SDIR, v1: UInt8, v2: UInt32)
+    {
+        let request = makeDirectModeRequest(ccon: ccon, cpos: cpos, cdir: cdir, v1: v1, v2: v2)
+        let responce = try modbus.writeAndReadRegisters(writeAddr: 0, data: request, readAddr: 0, readCount: 4)
+        let (scon, spos, sdir, v1, v2) = parceDirectModeResponce(responce)
+        guard !scon.contains([.fault, .warn]) else {
+            throw FestoError.faultOrWarn
+        }
+        return (scon, spos, sdir, v1, v2)
     }
 
     // T1
@@ -133,5 +147,4 @@ public class FestoModbus {
         }
         throw FestoError.unlock
     }
-
 }
